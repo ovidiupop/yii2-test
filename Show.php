7ef444791bbd7238071757fff091d995661f07ad
ovidiupop <ovidiupop70@gmail.com>
2015-07-10 10:10:04 +0300

    Update Show.php

--- Show.php
+++ Show.php
@@ -1,19 +1,14 @@
 <?php
-namespace ovidiupop\test;
+namespace matricks\test;
 
 use Yii;
 use yii\bootstrap\Widget;
 
 class Show extends Widget{
     
-<<<<<<< HEAD
     public $var = "ceva variabila";
     
     public function run(){
         return $this->render('uploader', ['var'=>$this->var]);;
-=======
-    public function run(){
-        echo "yes";
->>>>>>> 5152e03eb2d322bb77a76e152854187b8660f229
     }
 }
